550ff08e3f7ab6c5e92afdf29bbb447eff7bb2f1
Moch Fikri Setiawan <mixchannel307@gmail.com>
2024-10-08 19:21:00 +0700

    First Commits

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.template')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Halo apakabar</h3>
+            <div class="card-tools"></div>
+        </div>
+    </div>
+@endsection
